eae076024e4c98773766583563f5bb620360bc8d
Первый комит <suzdalcev97t@gmail.com>
2025-03-15 00:37:38 +0400

    Конец

--- src/Main.java
+++ src/Main.java
@@ -59,13 +59,40 @@ public class Main {
         System.out.println(weiqhKg +" кг");
 
 
+        byte weightKg = 7;
+        short weightGr = (short) (weightKg * grPerKg);
+        short twoHundredFifty = (short) (weightGr/250);
+        short fiveHundred = (short) (weightGr/500);
+        short averge = (short) ((twoHundredFifty + fiveHundred) / 2);
+        System.out.println( "При потере по 250 грамм в день потребуется " + twoHundredFifty + " дней ");
+        System.out.println( "При потере по 500 грамм в день потребуется " + fiveHundred + " дней ");
+        System.out.println( "В среднем чтобы добится результата потребуется " + averge + " день ");
 
 
 
+        int masha = 67760;
+        int denis = 83690;
+        int kristina = 76230;
 
+        int mashaBonus = (int) (masha * 1.1);
+        int denisBonus = (int) (denis * 1.1);
+        int kristinaBonus = (int) (kristina * 1.1);
 
+        int mashaYear = masha * 12;
+        int denisYear = denis * 12;
+        int kristinaYear = kristina * 12;
 
+        int mashaYearBonus = mashaBonus * 12;
+        int denisYearBonus = denisBonus * 12;
+        int kristinaYearBonus = kristinaBonus * 12;
 
+        int mashaDifference = mashaYearBonus - mashaYear;
+        int denisDifference = denisYearBonus - denisYear;
+        int kristinaDifference = kristinaYearBonus - kristinaYear;
+
+        System.out.println("Маша теперь получает "+ mashaBonus + " рублей. Годовой доход вырос на " + mashaDifference + " рублей ");
+        System.out.println("Денис теперь получает "+ denisBonus + " рублей. Годовой доход вырос на " + denisDifference + " рублей ");
+        System.out.println("Кристина теперь получает "+ kristinaBonus + " рублей. Годовой доход вырос на " + kristinaDifference + " рублей ");
 
     }
 }
